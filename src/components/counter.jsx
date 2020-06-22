b2a99848c4a82bb15482803859824bb16f8d092b
Margaret Kissi <magkissi111@gmail.com>
2020-06-22 17:18:33 +0100

    add reset button and Navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,33 +3,25 @@ import React, { Component } from 'react';
 
 // this references the current object. it is specific to its boundary//
 class Counter extends Component {
-    state = {
-        value: this.props.value
-        
-    };
-
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1});
-    };
-
+      
     render() { 
         return (
         <div>
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-          <button className="btn btn-danger btn-sm m-2">Delete</button>
+          <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+          <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
        </div>
    );
  }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
  formatCount() {
-     const { value } = this.state;
+     const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
  }
 }
